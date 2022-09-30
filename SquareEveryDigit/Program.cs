435d6d8e75d9457b0911403f0820be31f821b0bc
PrgaTest123 <marintest0@gmail.com>
2022-09-30 20:21:51 +0200

    Add project files.

---
+++ SquareEveryDigit/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace SquareEveryDigit
+{
+    
+   
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter Digit: ");
+            int digit  = Int32.Parse(Console.ReadLine());
+
+
+            Console.WriteLine("Each digit squared is: " + SquareDigit(digit));
+        }
+
+
+        public static int SquareDigit(int n)
+        {
+            String digits = n.ToString();
+            String result = "";
+            foreach (char c in digits)
+            {
+                double squared = Math.Pow(Int32.Parse(c.ToString()), 2);
+                result += squared.ToString();
+            }
+            return Int32.Parse(result);
+        }
+    }
+}
