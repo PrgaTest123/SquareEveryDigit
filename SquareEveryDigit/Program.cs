33b1d70b2a54229d72b41fd14ceadaa88d95ee46
PrgaTest123 <marintest0@gmail.com>
2022-09-30 20:32:57 +0200

    Added best practice

--- SquareEveryDigit/Program.cs
+++ SquareEveryDigit/Program.cs
@@ -19,6 +19,9 @@ namespace SquareEveryDigit
 
         public static int SquareDigit(int n)
         {
+            //best practice
+           // return int.Parse(String.Concat(n.ToString().Select(a => (int)Math.Pow(char.GetNumericValue(a), 2))));
+
             String digits = n.ToString();
             String result = "";
             foreach (char c in digits)
